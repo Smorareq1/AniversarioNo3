f2879933ffd275e299584ea8abe942e9c9738399
Smorareq1 <smorareq1@gmail.com>
2024-05-06 21:13:59 -0600

    Solo quedan cartas

---
+++ assets/js/reproductor.js
@@ -0,0 +1,34 @@
+let progress = document.getElementById('progress');
+let song = document.getElementById('song');
+let ctrlIcon = document.getElementById('ctrlIcon');
+
+song.onloadedmetadata = function() {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+function playPause() {
+   if(ctrlIcon.classList.contains('fa-pause')) {
+       song.pause();
+       ctrlIcon.classList.remove('fa-pause');
+       ctrlIcon.classList.add('fa-play');
+   }
+   else {
+       song.play();
+       ctrlIcon.classList.remove('fa-play');
+       ctrlIcon.classList.add('fa-pause');
+   }
+}
+
+// Actualizar el progreso mientras se reproduce la canción
+song.addEventListener('timeupdate', function() {
+    progress.value = song.currentTime;
+});
+
+progress.onchange = function() {
+    song.play();
+    song.currentTime = progress.value;
+    ctrlIcon.classList.add('fa-pause');
+    ctrlIcon.classList.remove('fa-play');
+
+}
